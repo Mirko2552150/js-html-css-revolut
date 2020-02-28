07c2a53bca5c2be92e510c46749e15f201a2ea98
Mirko <mirko.longo.1987@gmail.com>
2020-02-28 17:37:54 +0100

    Funziona ma da rivedere assolutamente

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
+// $('.menu-item-first .icon').click(function(){
+//     $( ".dropdown-menu-first.visibility" ).css("display", "block");
+// });
+//
+// $('.menu-item-first .icon').dblclick(function(){
+//     $( ".dropdown-menu-first.visibility" ).css("display", "none");
+// });
+//
+// $('.menu-item-second .icon').click(function(){
+//     $( ".dropdown-menu-second.visibility" ).css("display", "block");
+// });
+//
+// $('.menu-item-second .icon').dblclick(function(){
+//     $( ".dropdown-menu-second.visibility" ).css("display", "none");
+// });
+//
+// $('.menu-item-third .icon').click(function(){
+//     $( ".dropdown-menu-third.visibility" ).css("display", "block");
+// });
+//
+// $('.menu-item-third .icon').dblclick(function(){
+//     $( ".dropdown-menu-third.visibility" ).css("display", "none");
+// });
 
+$('.iconUno').click(function(){
+    $(".dropdown-menu-first").removeClass("visibility");
+});
+
+$('.iconUno').dblclick(function(){
+    $(".dropdown-menu-first").addClass("visibility");
+});
+
+$('.iconDue').click(function(){
+    $(".dropdown-menu-second").removeClass("visibility-due");
+});
+
+$('.iconDue').dblclick(function(){
+    $(".dropdown-menu-second").addClass("visibility-due");
+});
+
+$('.iconTre').click(function(){
+    $(".dropdown-menu-third").removeClass("visibility-tre");
+});
+
+$('.iconTre').dblclick(function(){
+    $(".dropdown-menu-third").addClass("visibility-tre");
+});
+
+/* DA RIVEDERE DOMANI */
