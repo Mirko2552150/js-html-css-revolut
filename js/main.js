9c15810b991c2477d3a720b7accfeb210139584c
Mirko <mirko.longo.1987@gmail.com>
2020-02-29 15:38:28 +0100

    commentato

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 
 $( ".icon" ).click(function() {
-  // $( ".dropdown" ).slideToggle(1000);
-  $(this).next('.dropdown').slideToggle(1000);
+  $(this).next('.dropdown').slideToggle(1000); // NEXT FRATELLO SUBUTO DOPO / SLIDE TOGGLE SLIDE DOWN + SLIDE UP (TEMPO IN ms)
 });
 
 $( ".icon" ).click(function() {
-  $(this).toggleClass("ruota");
+  $(this).toggleClass("ruota"); // this per selezionare QUELL? ELEMENTO TRA ELEMENTI UGUALI
 });
