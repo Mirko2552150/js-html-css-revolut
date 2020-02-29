fe37497ddb49a0346b8cbea19054612424a1f56c
Mirko <mirko.longo.1987@gmail.com>
2020-02-29 15:35:19 +0100

    Completo

--- js/main.js
+++ js/main.js
@@ -1,49 +1,9 @@
-// $('.menu-item-first .icon').click(function(){
-//     $( ".dropdown-menu-first.visibility" ).css("display", "block");
-// });
-//
-// $('.menu-item-first .icon').dblclick(function(){
-//     $( ".dropdown-menu-first.visibility" ).css("display", "none");
-// });
-//
-// $('.menu-item-second .icon').click(function(){
-//     $( ".dropdown-menu-second.visibility" ).css("display", "block");
-// });
-//
-// $('.menu-item-second .icon').dblclick(function(){
-//     $( ".dropdown-menu-second.visibility" ).css("display", "none");
-// });
-//
-// $('.menu-item-third .icon').click(function(){
-//     $( ".dropdown-menu-third.visibility" ).css("display", "block");
-// });
-//
-// $('.menu-item-third .icon').dblclick(function(){
-//     $( ".dropdown-menu-third.visibility" ).css("display", "none");
-// });
 
-$('.iconUno').click(function(){
-    $(".dropdown-menu-first").removeClass("visibility");
+$( ".icon" ).click(function() {
+  // $( ".dropdown" ).slideToggle(1000);
+  $(this).next('.dropdown').slideToggle(1000);
 });
 
-$('.iconUno').dblclick(function(){
-    $(".dropdown-menu-first").addClass("visibility");
+$( ".icon" ).click(function() {
+  $(this).toggleClass("ruota");
 });
-
-$('.iconDue').click(function(){
-    $(".dropdown-menu-second").removeClass("visibility-due");
-});
-
-$('.iconDue').dblclick(function(){
-    $(".dropdown-menu-second").addClass("visibility-due");
-});
-
-$('.iconTre').click(function(){
-    $(".dropdown-menu-third").removeClass("visibility-tre");
-});
-
-$('.iconTre').dblclick(function(){
-    $(".dropdown-menu-third").addClass("visibility-tre");
-});
-
-/* DA RIVEDERE DOMANI */
